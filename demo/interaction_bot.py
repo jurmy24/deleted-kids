7adaaadd9e97013bc36e5bec46a16792bf081684
Victor Oldensand <victormag24@gmail.com>
2024-09-03 17:18:32 +0200

    Feat: make singleton

--- demo/interaction_bot.py
+++ demo/interaction_bot.py
@@ -2,13 +2,30 @@ from pydantic import UUID4
 from vapi_python import Vapi
 from dotenv import load_dotenv
 import os
+import threading
 
 
 class InteractionBot:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(InteractionBot, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        load_dotenv()  # Load environment variables from .env file
-        api_key = os.getenv("VAPI_PUBLIC_KEY")
-        self.vapi = Vapi(api_key=api_key)
+        if not hasattr(self, "_initialized"):
+            load_dotenv()  # Load environment variables from .env file
+            api_key = os.getenv("VAPI_PUBLIC_KEY")
+            if api_key is None:
+                raise ValueError(
+                    "API key for Vapi is not set in the environment variables."
+                )
+            self.vapi = Vapi(api_key=api_key)
+            self._initialized = True
 
     def start_interaction(self, assistant_id: UUID4, first_message: str, context: str):
         """Start the interactive session using Vapi."""
